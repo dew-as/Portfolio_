df26547d3dfd162d4e3b5af796ad67eb35f29c44
Aswindev <146868243+dew-as@users.noreply.github.com>
2024-11-07 18:10:27 +0530

    Update timeline.js

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,14 +1,14 @@
 export default [
   {
-    skill: "Frontend",
-    title: "React JS",
+    skill: "REACT",
+    title: "Frontend",
     details:
       "As a Full-Stack Developer with experience in HTML, CSS, JavaScript, React, and Tailwind CSS, I bring a comprehensive skill set to the table. My goal is to build application that are beautiful, accessible, easy to use, and maintainable. I am dedicated to creating a smooth and intuitive user experience, ensuring that the application is user-friendly and responsive across different devices and screen sizes.",
   },
   {
-    skill: "Backend",
-    title: "Next JS",
+    skill: "NODE,EXPRESS,MONGO",
+    title: "Backend",
     details:
-      "As a Full-Stack Developer, I have a strong focus on backend development and possess extensive skills and experience in Node.js/Express.js and MongoDB databases. I understand the importance of writing clean and maintainable code, which allows for easier collaboration and future enhancements. With my strong backend skills and experience, I can design and develop efficient APIs that deliver data securely and efficiently, ensuring optimal performance and a smooth user experience.",
+      "A skilled Node.js developer with extensive experience building and maintaining scalable, high-performance applications using Express.js for server-side development and MongoDB for data storage. Proficient in designing RESTful APIs, implementing secure user authentication (JWT), and managing efficient data workflows. Strong understanding of backend best practices, including middleware configuration, error handling, and asynchronous programming. Experienced in integrating third-party services, optimizing database queries, and using Mongoose for schema modeling and validation. Dedicated to writing clean, maintainable code and deploying applications with a focus on reliability and scalability.",
   },
 ];
